0cc09375eb4a4fa0c48c724ba54bc208ef02343c
Matthew Shane Rogers <matthew.shane.rogers@gmail.com>
2021-02-24 14:17:57 -0800

    profile.js: added JSONPretty package to display user information rather than verifying it with JSON.stringify()

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import LoginButton from './components/loginButton';
 import LogoutButton from './components/logoutButton';
+import Profile from './components/profile';
 
 function App() {
   return (
     <>
       <LoginButton />
       <LogoutButton />
+      <Profile />
     </>
   );
 }
