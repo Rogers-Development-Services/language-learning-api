ee5175e3c4a6b051aef550424b82d76779ee29c5
Matthew Shane Rogers <matthew.shane.rogers@gmail.com>
2021-02-24 14:30:57 -0800

    app.js: added isLoading compoent to prevent rerendering of loginbutton to logoutbutton loginButton.js & logoutbutton.js: used isAuthenticated from auth0 hook to conditionally render each button.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import './App.css';
 import LoginButton from './components/loginButton';
 import LogoutButton from './components/logoutButton';
 import Profile from './components/profile';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>
+
   return (
     <>
       <LoginButton />
--- src/components/profile.js
+++ src/components/profile.js
@@ -3,13 +3,18 @@ import { useAuth0 } from '@auth0/auth0-react';
 import JSONPretty from 'react-json-pretty';
 
 const Profile = () => {
-    const { user } = useAuth0();
+    const { user, isAuthenticated } = useAuth0();
 
     return (
-        <div>
-            <JSONPretty data={user} />
-            {/* {JSON.stringify(user, null, 2)} */}
-        </div>
+        isAuthenticated && (
+            <div>
+                <img src={user.picture} alt={user.name} />
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+                <JSONPretty data={user} />
+                {/* {JSON.stringify(user, null, 2)} */}
+            </div>
+        )
     )
 }
 
